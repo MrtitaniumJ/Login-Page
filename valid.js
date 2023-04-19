9922113b1c13bea7b53a048751f0c52efd1c7094
Yatharth Mishra <yatharth.mishra2002@gmail.com>
2023-04-20 00:34:20 +0530

    several changes

--- valid.js
+++ valid.js
@@ -1,10 +1,10 @@
 	//Validtion Code For Inputs
 
-var email = document.forms['form']['email'];
-var password = document.forms['form']['password'];
+var email = document.forms['form']['loginUser'];
+var password = document.forms['form']['loginPassword'];
 
-var email_error = document.getElementById('email_error');
-var pass_error = document.getElementById('pass_error');
+// var email_error = document.getElementById('email_error');
+// var pass_error = document.getElementById('pass_error');
 
 email.addEventListener('textInput', email_Verify);
 password.addEventListener('textInput', pass_Verify);
@@ -26,14 +26,12 @@ function validated(){
 }
 function email_Verify(){
 	if (email.value.length >= 8) {
-		email.style.border = "1px solid silver";
 		email_error.style.display = "none";
 		return true;
 	}
 }
 function pass_Verify(){
 	if (password.value.length >= 5) {
-		password.style.border = "1px solid silver";
 		pass_error.style.display = "none";
 		return true;
 	}
